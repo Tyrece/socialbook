b8beb8fd591c33547e39ab7df325961bd4a37b75
Tyrece <tyreceb14@gmail.com>
2019-01-31 15:49:37 -0400

    added photo and updated html/css

--- client/main.js
+++ client/main.js
@@ -3,20 +3,16 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+
+Template.profile.events({
+  'click .js-like'(event, instance) {
+    console.log("you clicked like")
+  },
+  'click .js-dis'(event, instance) {
+    alert("you disliked")
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+
+
