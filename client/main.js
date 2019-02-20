09575d315401f205ba3ae4617d2d7b82d958f54f
Tyrece <tyreceb14@gmail.com>
2019-02-20 15:39:21 -0400

    delete icon, fade out, etc

--- client/main.js
+++ client/main.js
@@ -4,7 +4,11 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 import '../lib/Collections.js';
 
-
+Template.profile.helpers({
+	profAll(){
+		return userDB.find({});
+	}
+});
 
 Template.profile.events({
   'click .js-like'(event, instance) {
@@ -13,26 +17,36 @@ Template.profile.events({
   'click .js-dis'(event, instance) {
     alert("you clicked dislike")
   },
+  'click .js-delete' (event, instance){
+  	//console.log(this._id);
+  	var profID = this._id;
+  	$("#" + this._id).fadeOut("slow","swing",function () {
+  	userDB.remove({_id: this._id});
+  });
+
+  }
 
 });
+
+
 
 
 Template.addprofile.events({
 	'click .js-add'(event, instance){
 	var fName = $("#exampleModal input[name='firstName']").val();
-	console.log("The First Name is",fName);
-
 	var lName = $("#exampleModal input[name='lastName']").val();
-	console.log("The Last Name is",lName);
-
 	var Ppic = $("#exampleModal input[name='Profilepic']").val();
-	console.log("The Profile Picture is",Ppic);
+	if (Ppic == ""){
+		Ppic="v.jpg";
+	}
+	//console.log("The Profile Picture is",Ppic);
 
 	 $("#exampleModal input[name='firstName']").val('');
 	 $("#exampleModal input[name='lastName']").val('');
 	 $("#exampleModal input[name='Profilepic']").val('');
 	 
   	$("#exampleModal").modal("hide");
+  	userDB.insert({'firstName':fName, 'lastName': lName, 'Profilepic': Ppic});
 
   },
 
