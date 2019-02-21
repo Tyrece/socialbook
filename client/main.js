6dc2f54e537f48c272e462e9886d44734ff6e4db
Tyrece <tyreceb14@gmail.com>
2019-02-21 15:39:30 -0400

    put counter on like/dislike button

--- client/main.js
+++ client/main.js
@@ -13,15 +13,31 @@ Template.profile.helpers({
 Template.profile.events({
   'click .js-like'(event, instance) {
     alert("you clicked like")
+    var profID = this._id;
+    var numLikes = userDB.findOne({_id: profID}).like;
+    if (!numLikes){
+    	numLikes = 0;
+    }
+    console.log("you have",numLikes);
+    userDB.update({_id:profID}, {$set:{'like':numLikes + 1}});
   },
   'click .js-dis'(event, instance) {
     alert("you clicked dislike")
+    var profID = this._id;
+    var numLikes = userDB.findOne({_id: profID}).dislike;
+    if (!numLikes){
+    	numLikes = 0;
+	 }
+    	console.log("you have",numLikes);
+    userDB.update({_id:profID}, {$set:{'dislike':numLikes + 1}});
   },
+
+
   'click .js-delete' (event, instance){
-  	//console.log(this._id);
+  	console.log(this._id);
   	var profID = this._id;
-  	$("#" + this._id).fadeOut("slow","swing",function () {
-  	userDB.remove({_id: this._id});
+  	$("#" + profID).fadeOut("slow","swing",function () {
+  	userDB.remove({_id: profID});
   });
 
   }
@@ -39,7 +55,9 @@ Template.addprofile.events({
 	if (Ppic == ""){
 		Ppic="v.jpg";
 	}
-	//console.log("The Profile Picture is",Ppic);
+	console.log("The Profile Picture is",Ppic);
+	console.log("The Profile firstName is",fName);
+	console.log("The Profile lastName is",lName);
 
 	 $("#exampleModal input[name='firstName']").val('');
 	 $("#exampleModal input[name='lastName']").val('');
